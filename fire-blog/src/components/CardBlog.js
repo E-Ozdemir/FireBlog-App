2892d81dc892d243f34bdcf9e6794d95f8b24c8c
E-Ozdemir <emreclarusway@gmail.com>
2021-09-24 02:01:05 +0200

    card component created

---
+++ fire-blog/src/components/CardBlog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CardComp from './CardComp'
+
+
+const CardBlog = () => {
+    return (
+        <div>
+            <CardComp/>
+        </div>
+    )
+}
+
+export default CardBlog
